bf7c60104aa1a0bcd6b193800b23a26415eb34f4
luke-hagar-sp <98849695+luke-hagar-sp@users.noreply.github.com>
2022-08-24 08:20:58 -0500

    Update stunt.sh

--- stunt.sh
+++ stunt.sh
@@ -97,11 +97,11 @@ function lookFor() {
 # $3 == Description of the test
 # $4 == original look for message, to preserve the log file structure
 checkResponse() {
-  result=$( grep -e "$2" $1 )
+  result=$( grep -e "$2" "$1" )
   if [ -n "$result" ]; then
-      $commandOutput+="$3 Test Succeeded"
+      $commandOutput += "$3 Test Succeeded"
   else
-      $commandOutput+="$3 Test Failed"
+      $commandOutput += "$3 Test Failed"
   fi
   lookFor $4 
   $1 >> $LOGFILE
